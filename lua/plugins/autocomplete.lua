37c12fc966f79e7b3ad3ab7c1bff936cc5e2dc89
WasathTheekshana <wasath.vt@gmail.com>
2024-05-31 17:57:40 +0530

    refactor: Keybind changes

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,81 +1,86 @@
 return {
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-      },
-      'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-    },
-    config = function()
-      -- See `:help cmp`
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      luasnip.config.setup {}
+	{ -- autocompletion
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{
+				"l3mon4d3/luasnip",
+				build = (function()
+					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+						return
+					end
+					return "make install_jsregexp"
+				end)(),
+				dependencies = {
+					{
+						"rafamadriz/friendly-snippets",
+						config = function()
+							require("luasnip.loaders.from_vscode").lazy_load()
 
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = { completeopt = 'menu,menuone,noinsert' },
+							-- Extend filetypes
+							local ls = require("luasnip")
+							ls.filetype_extend("javascript", { "javascriptreact" })
+							ls.filetype_extend("typescript", { "typescriptreact" })
+						end,
+					},
+				},
+			},
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			-- see `:help cmp`
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			luasnip.config.setup({})
 
-        -- Autocompletion
-        -- Please read `:help ins-completion`
-        mapping = cmp.mapping.preset.insert {
-          -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				completion = { completeopt = "menu,menuone,noinsert" },
 
-          -- Scroll the documentation window [b]ack / [f]orward
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+				-- autocompletion
+				-- please read `:help ins-completion`
+				mapping = cmp.mapping.preset.insert({
+					-- select the [n]ext item
+					["<c-n>"] = cmp.mapping.select_next_item(),
+					-- select the [p]revious item
+					["<c-p>"] = cmp.mapping.select_prev_item(),
 
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+					-- scroll the documentation window [b]ack / [f]orward
+					["<c-b>"] = cmp.mapping.scroll_docs(-4),
+					["<c-f>"] = cmp.mapping.scroll_docs(4),
 
-          -- Traditional completion navigation
-          ['<CR>'] = cmp.mapping.confirm { select = true },
-          ['<Tab>'] = cmp.mapping.select_next_item(),
-          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+					["<c-y>"] = cmp.mapping.confirm({ select = true }),
 
-          ['<C-Space>'] = cmp.mapping.complete {},
+					-- traditional completion navigation
+					["<cr>"] = cmp.mapping.confirm({ select = true }),
+					["<tab>"] = cmp.mapping.select_next_item(),
+					["<s-tab>"] = cmp.mapping.select_prev_item(),
 
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-        },
-      }
-    end,
-  },
+					["<c-space>"] = cmp.mapping.complete({}),
+
+					["<c-l>"] = cmp.mapping(function()
+						if luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						end
+					end, { "i", "s" }),
+					["<c-h>"] = cmp.mapping(function()
+						if luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						end
+					end, { "i", "s" }),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "path" },
+				},
+			})
+		end,
+	},
 }
