da78526791062ffe67dbbe456daba371ee3bc389
WasathTheekshana <wasath.vt@gmail.com>
2024-06-16 21:40:53 +0530

    refactor: Notify background color change

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,33 +1,44 @@
 return {
-  'folke/noice.nvim',
-  event = 'VeryLazy',
-  enabled = true,
-  opts = {},
-  dependencies = {
-    'MunifTanjim/nui.nvim',
-    'rcarriga/nvim-notify',
-  },
-  config = function()
-    require('noice').setup {
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true,
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true, -- add a border to hover docs and signature help
-      },
-      -- cmdline = {
-      --   view = 'cmdline',
-      -- },
-    }
-  end,
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	enabled = true,
+	opts = {},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	config = function()
+		-- Define a highlight group with a background color
+		vim.cmd("highlight NotifyBackground guibg=#000000")
+
+		-- Require and setup 'notify' with the background color highlight group
+		local notify = require("notify")
+		notify.setup({
+			background_colour = "NotifyBackground", -- Use the defined highlight group
+		})
+
+		-- Additional configurations for 'noice.nvim' can go here if needed
+		require("noice").setup({
+			lsp = {
+				-- Override markdown rendering to use Treesitter for **cmp** and other plugins
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			-- Enable presets for easier configuration
+			presets = {
+				bottom_search = true, -- Use a classic bottom cmdline for search
+				command_palette = true, -- Position the cmdline and popupmenu together
+				long_message_to_split = true, -- Send long messages to a split
+				inc_rename = false, -- Disable an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- Add a border to hover docs and signature help
+			},
+			-- Example of adding a new configuration:
+			-- notifications = {
+			--   background_colour = "#1e222a",  -- Set a custom background color for notifications
+			-- },
+		})
+	end,
 }
